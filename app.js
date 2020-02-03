a3f53939c26a5a4575aabbbf04e9b52d4fbef4c4
Царицын Андрей Андреевич <aa_tsaritsyn@bbgroup.local>
2020-02-03 08:54:14 +0300

    1й коммит в 15 пр.

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 const express = require('express');
+require('dotenv').config();
 const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
-const { celebrate, Joi } = require('celebrate');
+const { celebrate, Joi, errors } = require('celebrate');
 const cardsRout = require('./routes/cards');
 const userRout = require('./routes/users');
 const auth = require('./middlewares/auth');
 const { createUser, login } = require('./controllers/users');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
+
+console.log(process.env.NODE_ENV);
 
 const app = express();
 app.use(cookieParser());
@@ -23,6 +27,7 @@ app.get('/posts', (req, res) => console.log(`Токен: ${req.cookies.jwt}`));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(requestLogger);
 
 app.post('/signup', celebrate({
   body: Joi.object().keys({
@@ -33,12 +38,26 @@ app.post('/signup', celebrate({
     email: Joi.string().required().email(),
   }),
 }), createUser);
-app.post('/signin', login);
+app.post('/signin', celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().required().email(),
+    password: Joi.string().required().min(8),
+  }),
+}), login);
 
 app.use(auth);
 
 app.use('/', userRout);
 app.use('/', cardsRout);
-app.use((req, res) => res.status(404).send({ message: 'Запрашиваемый ресурс не найден' }));
+
+app.use(errorLogger);
+
+app.use(errors());
+
+app.use((err, req, res, next) => {
+  const { statusCode = 500, message } = err;
+  res.status(statusCode).send({ message: statusCode === 500 ? 'На сервере произошла ошибка' : message });
+  next();
+});
 
 app.listen(PORT, () => console.log(`Используется порт ${PORT}`));
--- controllers/cards.js
+++ controllers/cards.js
@@ -1,7 +1,7 @@
 const Card = require('../models/card');
 const MyError = require('../modules/error');
 
-module.exports.createCard = (req, res) => {
+module.exports.createCard = (req, res, next) => {
   const {
     name, link, likes,
   } = req.body;
@@ -10,10 +10,10 @@ module.exports.createCard = (req, res) => {
     name, link, owner, likes,
   })
     .then((card) => res.send({ card }))
-    .catch((err) => res.status(500).send({ message: err }));
+    .catch(next);
 };
 
-module.exports.deleteCard = (req, res) => {
+module.exports.deleteCard = (req, res, next) => {
   const { id } = req.params;
   Card.findById(id)
     .then((card) => {
@@ -21,17 +21,19 @@ module.exports.deleteCard = (req, res) => {
         throw new MyError('Объект не найден', 402);
       }
       if (card.owner !== req.user._id) {
-        throw new MyError('Недостаточно прав', 403);
+        next(new MyError('Недостаточно прав', 403));
       } else {
         Card.findByIdAndDelete(id)
-          .then((deletedcard) => res.send({ deletedcard }));
+          .then((deletedcard) => res.send({ deletedcard }))
+          .catch(next);
       }
     })
-    .catch((err) => res.status(err.code || 500).json({ message: err.message }));
+    .then((card) => res.send({ card }))
+    .catch(next);
 };
 
-module.exports.getCards = (req, res) => {
+module.exports.getCards = (req, res, next) => {
   Card.find({})
     .then((cards) => res.send({ cards }))
-    .catch((err) => res.status(500).send({ message: err }));
+    .catch(next);
 };
